3d2f67bd9cde20536b307b2f3e0f534a62a0ab83
Giovani Felipe Guimaraes Santos <giovani_santos@outlook.com>
2020-03-31 20:10:26 -0300

    Find client by ID

--- server.js
+++ server.js
@@ -18,7 +18,8 @@ const Cliente = new Operacoes("cliente");
 const resolvers = {
   Query: {
     status: () => "Servidor rodando!",
-    clientes: () => Cliente.lista()
+    clientes: () => Cliente.lista(),
+    cliente: (root, { id }) => Cliente.buscaPorId(id)
   },
   Mutation: {
     adicionarCliente: (root, params) => Cliente.adiciona(params)
