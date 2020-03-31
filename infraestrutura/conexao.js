a0bc9aa9af566ec7bba151b8e0d43b4e7d71e6ca
Giovani Felipe Guimaraes Santos <giovani_santos@outlook.com>
2020-03-30 21:47:59 -0300

    Graphql init

--- infraestrutura/conexao.js
+++ infraestrutura/conexao.js
@@ -3,8 +3,8 @@ const mysql = require('mysql')
 const conexao = mysql.createConnection({
   host: 'localhost',
   port: 3306,
-  user: 'root',
-  password: 'admin',
+  user: 'mysql',
+  password: 'mysql',
   database: 'agenda-petshop'
 })
 
--- server.js
+++ server.js
@@ -1,19 +1,26 @@
-const customExpress = require('./config/custom-express')
-const conexao = require('./infraestrutura/conexao')
-const Tabelas = require('./infraestrutura/database/tabelas')
-
-const app = customExpress()
+const { GraphQLServer } = require("graphql-yoga");
+const conexao = require("./infraestrutura/conexao");
+const Tabelas = require("./infraestrutura/database/tabelas");
 
 conexao.connect(erro => {
   if (erro) {
-    console.log(erro)
+    console.log(erro);
   }
 
-  console.log('conectou no banco')
+  console.log("conectou no banco");
 
-  Tabelas.init(conexao)
-})
+  Tabelas.init(conexao);
+});
 
-app.listen(4000, () => {
-  console.log('Servidor rodando na porta 4000')
-})
+const resolvers = {
+  Query: {
+    status: () => "Servidor rodando!"
+  }
+};
+
+const servidor = new GraphQLServer({
+  typeDefs: "./schema.graphql",
+  resolvers
+});
+
+servidor.start(() => console.log("servidro ouvindo"));
